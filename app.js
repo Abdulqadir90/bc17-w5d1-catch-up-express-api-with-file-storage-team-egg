3632f71381826b14fa6fc201bb789efbff2b7c95
Elspeth <elspeth@mardaljevic.org>
2024-07-29 11:45:53 +0100

    first get request

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 import express from "express";
+// import helmet
+import helmet from 'helmet';
+
 
 import {
   getRecipes,
@@ -14,6 +17,34 @@ const PORT = 3000;
 app.use(express.static("public"));
 app.use(express.json());
 
+// helmet 
+app.use(helmet());
+
+// Ask Helmet to ignore the X-Powered-By header.
+app.use(
+  helmet({
+    xPoweredBy: false,
+  })
+);
+
+// GET request to get all recipes
+app.get('/recipes', (req, res) => {
+  try {
+    res.status(200).json({
+      'success': 'egg 🥚',
+      'payload': null
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      'success': false,
+      'payload': null
+    })
+  }
+})
+
+
+// Event listener that listens out for any requests made by client.
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
